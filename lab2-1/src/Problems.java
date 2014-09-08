24390e46e1e63caf954f28a3a1b1db80256019bd
Joe <mooman219@gmail.com>
2014-09-08 11:08:45 -0400

    Fix for loop from going out of bounds

--- lab2-1/src/Problems.java
+++ lab2-1/src/Problems.java
@@ -7,7 +7,7 @@ public class Problems {
          less.
          */
         int inRange = 0;
-        for (int i = 0; i <= nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if ((nums[i] >= value - 2) && (nums[i] <= value + 2)) {
                 inRange++;
             }
