8edce745c3db5a59a29fd8fc971a191cf3f2a010
Joe <mooman219@gmail.com>
2014-09-08 11:47:39 -0400

    Add test output

--- lab2-2/src/Negative.java
+++ lab2-2/src/Negative.java
@@ -10,5 +10,15 @@ public class Negative {
             System.out.println("usage: java Negative JPGfile");
             return;
         }
+        System.out.println("width: {" + picture.getWidth() + "}");
+        System.out.println("height: {" + picture.getHeight() + "}");
+        System.out.println("number of loop cycles: {" + "temp" + "}");
+        System.out.println("picture: Picture, filename " + picture.getFileName()
+                + " height " + picture.getHeight()
+                + " width " + picture.getWidth());
+//              width: {480}
+//              height: {360}
+//              number of loop cycles: {172800}
+//              picture: Picture, filename swan.jpg height 360 width 480
     }
 }
