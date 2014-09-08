2012a79d5a6dc3190d454b284e632b160267f93e
Joe <mooman219@gmail.com>
2014-09-08 11:37:22 -0400

    Add initial check for user input

--- lab2-2/src/Negative.java
+++ lab2-2/src/Negative.java
@@ -5,6 +5,10 @@
 public class Negative {
 
     public static void main(String[] args) {
-
+        Picture picture = new Picture();
+        if (args.length < 1 || !picture.load(args[0])) {
+            System.out.println("usage: java Negative JPGfile");
+            return;
+        }
     }
 }
