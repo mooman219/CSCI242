ed6104a6e402f4aa1b5daee8755f7751bad7d9c2
Joe <mooman219@gmail.com>
2014-09-09 16:25:21 -0400

    Change pixel loop to iterate through each row/col

--- lab2-3/src/Negative.java
+++ lab2-3/src/Negative.java
@@ -22,10 +22,11 @@ public class Negative {
                 + " height " + picture.getHeight()
                 + " width " + picture.getWidth());
 
-        for (Pixel pixel : picture.getPixels()) {
-            pixel.setRed(255 - pixel.getRed());
-            pixel.setGreen(255 - pixel.getGreen());
-            pixel.setBlue(255 - pixel.getBlue());
+        int width = (int) (picture.getWidth() * 0.025);
+        int height = (int) (picture.getHeight() * 0.025);
+        for (int x = 0; x < picture.getWidth(); x++) {
+            for (int y = 0; y < picture.getHeight(); y++) {
+            }
         }
 
         picture.show();
