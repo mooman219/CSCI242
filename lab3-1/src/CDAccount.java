9b577e000b382fac4b9df87d122ae578dd3a58cc
Joe <mooman219@gmail.com>
2014-09-15 11:24:37 -0400

    Implement BankAccount

--- lab3-1/src/CDAccount.java
+++ lab3-1/src/CDAccount.java
@@ -1,6 +1,28 @@
+
 /**
  * @author Joseph Cumbo
  */
-public class CDAccount {
+public class CDAccount extends BankAccount {
+
+    public static double MINIMUM_BALANCE = 1000;
+    public static double MONTHLY_INTEREST_RATE = 0.06;
+
+    public CDAccount(double newMoney, String ownerName) {
+        super(newMoney, ownerName);
+    }
+
+    @Override
+    public void calcInterest() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Account Type: CD " + super.toString();
+    }
 
 }
