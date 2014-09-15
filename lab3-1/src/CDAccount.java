0c52a58299557990e9003f50d2831c0b6d7cf750
Joe <mooman219@gmail.com>
2014-09-15 11:40:23 -0400

    Document CDAccount

--- lab3-1/src/CDAccount.java
+++ lab3-1/src/CDAccount.java
@@ -4,13 +4,31 @@
  */
 public class CDAccount extends BankAccount {
 
+    /**
+     * The minimum amount that must be kept in the account at all times;
+     * interest is earned only on the amount over the minimum - currently $1000.
+     */
     public static double MINIMUM_BALANCE = 1000;
+    /**
+     * The interest rate for balances greater than the minimum - currently 6%.
+     */
     public static double MONTHLY_INTEREST_RATE = 0.06;
 
+    /**
+     * A constructor for a CDAccount object. It accepts the amount of money
+     * deposited (the balance) when the account is created.
+     *
+     * @param newMoney The amount of money deposited when the account is opened
+     * @param ownerName The owner of this account
+     */
     public CDAccount(double newMoney, String ownerName) {
         super(newMoney, ownerName);
     }
 
+    /**
+     * Calculate interest for this account. The rules for earning interest are
+     * different for every kind of account.
+     */
     @Override
     public void calcInterest() {
         double growableBalance = getCurrentBalance() - MINIMUM_BALANCE;
