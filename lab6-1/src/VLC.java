14abc62bddabe11bfd2d81bc4d8ab54e465362ae
Joe <mooman219@gmail.com>
2014-10-07 17:43:57 -0400

    Parse user input in VLC

--- lab6-1/src/VLC.java
+++ lab6-1/src/VLC.java
@@ -1,6 +1,30 @@
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * @author Joseph Cumbo
  */
 public class VLC {
 
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String input = "";
+        while (true) {
+            System.out.print("Please enter symbol filename: ");
+            input = scanner.nextLine();
+            if (input.length() == 0) {
+                System.out.println("Invalid file name.");
+            } else {
+                break;
+            }
+        }
+
+        File file = new File(input);
+        if (!file.exists()) {
+            System.out.println("File '" + input + "' does not exist.");
+            return;
+        }
+
+    }
 }
