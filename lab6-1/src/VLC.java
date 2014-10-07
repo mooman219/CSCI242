0d933194b1cc28401f9bcaf2fa8c542ef4b57d02
Joe <mooman219@gmail.com>
2014-10-07 17:55:00 -0400

    Document the Symbol class

--- lab6-1/src/VLC.java
+++ lab6-1/src/VLC.java
@@ -34,26 +34,52 @@ public class VLC {
 
     public static class Symbol implements Comparable<Symbol> {
 
+        // The caracter this class represents
         private final char character;
+        // The frequency of the 'character'
         private final int frequency;
 
+        /**
+         * Creates a symbol class.
+         *
+         * @param character the 'character' this class is representing.
+         * @param frequency the frequency of the given 'character'.
+         */
         public Symbol(char character, int frequency) {
             this.character = character;
             this.frequency = frequency;
         }
 
+        /**
+         * Gets the character this symbol is representing.
+         *
+         * @return the character representing this class.
+         */
         public char getCharacter() {
             return character;
         }
 
+        /**
+         * Gets the frequency of the character this class represents.
+         *
+         * @return the frequency of the character this class represents.
+         */
         public int getFrequency() {
             return frequency;
         }
 
+        /**
+         * Compares this symbol to a given symbol.
+         *
+         * @param sym the symbol that will be compared against.
+         * @return -1 if the frequency of 'sym' is less than the frequency of
+         * this class. 0 if the frequencies are equal. 1 if the frequency of
+         * 'sym' is greater than this class.
+         */
         @Override
-        public int compareTo(Symbol o) {
-            return o.getFrequency() < frequency
-                    ? -1 : o.getFrequency() > frequency
+        public int compareTo(Symbol sym) {
+            return sym.getFrequency() < frequency
+                    ? -1 : sym.getFrequency() > frequency
                             ? 1 : 0;
         }
     }
