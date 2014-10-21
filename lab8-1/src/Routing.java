dfc00989e548012e2f11af6819f7c0dd97f07478
Joe <mooman219@gmail.com>
2014-10-21 17:50:42 -0400

    Take user input in Routing

--- lab8-1/src/Routing.java
+++ lab8-1/src/Routing.java
@@ -1,13 +1,47 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @author Joseph Cumbo
  */
 public class Routing {
 
-    /**
-     * @param args the command line arguments
-     */
+    // Scanner used for taking user input.
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        //
+        // Process the user's input
+        //
+        String input = "";
+        while (true) {
+            System.out.print("Please enter a filename: ");
+            input = scanner.nextLine();
+            if (input.length() == 0) {
+                System.out.println("Invalid file name.");
+            } else {
+                break;
+            }
+        }
+        System.out.println("");
+        //
+        // Check if the file exists
+        //
+        if (!new File(input).exists()) {
+            System.out.println("File '" + input + "' does not exist.");
+            return;
+        }
+        //
+        // Create the Graph
+        //
+        Graph graph = null;
+        try {
+            graph = new Graph(input);
+        } catch (IOException ex) {
+            System.out.println("Unable to read file.");
+            return;
+        }
     }
 
 }
