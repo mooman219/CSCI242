bc54b58acca23a3d5fd2bc593b6f730b17e40767
Joe <mooman219@gmail.com>
2014-11-04 20:38:07 -0500

    Implement HashDriver

--- lab10-1/src/HashDriver.java
+++ lab10-1/src/HashDriver.java
@@ -1,3 +1,7 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author Joseph Cumbo (mooman219)
@@ -8,7 +12,23 @@ public class HashDriver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        if (args.length > 0) {
+            try {
+                HashTable simpleTable = new HashTable(Hash.Htype.SIMPLE, 100000);
+                HashTable customTable = new HashTable(Hash.Htype.CUSTOM, 100000);
+                Scanner scanner = new Scanner(new FileReader(args[0]));
+                while (scanner.hasNext()) {
+                    String word = scanner.next();
+                    simpleTable.put(word);
+                    customTable.put(word);
+                }
+                System.out.println(simpleTable.imbalance());
+                System.out.println(customTable.imbalance());
+            } catch (FileNotFoundException ex) {
+                System.out.println("Unable to find file '" + args[0] + "'.");
+            }
+        } else {
+            System.out.println("Usage: filename.txt");
+        }
     }
-
 }
