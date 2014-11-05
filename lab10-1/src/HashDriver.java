b8a3295d42a6a1ecd00f7bc8fcfdf3942aed9622
Joe <mooman219@gmail.com>
2014-11-04 21:56:12 -0500

    Write the Driver class

--- lab10-1/src/HashDriver.java
+++ lab10-1/src/HashDriver.java
@@ -12,18 +12,21 @@ public class HashDriver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Buiding table for: " + args[0]);
         if (args.length > 0) {
-            try {
-                HashTable simpleTable = new HashTable(Hash.Htype.SIMPLE, 100000);
-                HashTable customTable = new HashTable(Hash.Htype.CUSTOM, 100000);
-                Scanner scanner = new Scanner(new FileReader(args[0]));
+            HashTable simpleTable = new HashTable(Hash.Htype.SIMPLE, 100000);
+            HashTable customTable = new HashTable(Hash.Htype.CUSTOM, 100000);
+            try (Scanner scanner = new Scanner(new FileReader(args[0]));) {
+                int words = 0;
                 while (scanner.hasNext()) {
                     String word = scanner.next();
                     simpleTable.put(word);
                     customTable.put(word);
+                    words++;
                 }
-                System.out.println(simpleTable.imbalance());
-                System.out.println(customTable.imbalance());
+                System.out.println("Total words: " + words);
+                System.out.println("Simple hash imbalance: " + simpleTable.imbalance());
+                System.out.println("Custom hash imbalance: " + customTable.imbalance());
             } catch (FileNotFoundException ex) {
                 System.out.println("Unable to find file '" + args[0] + "'.");
             }
