5288823284f318c8d668dd7780b1330b40801021
Joe <mooman219@gmail.com>
2014-11-06 20:42:07 -0500

    Implement Clock

--- project2-1/src/Clock.java
+++ project2-1/src/Clock.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  *
  * @author Joseph Cumbo (mooman219)
@@ -8,7 +10,55 @@ public class Clock implements Puzzle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+    }
+
+    private final int total;
+    private final int start;
+    private final int goal;
+
+    public Clock(int total, int start, int goal) {
+        this.total = total;
+        this.start = start;
+        this.goal = goal;
+    }
+
+    /**
+     * Get the starting config for this puzzle.
+     *
+     * @return the starting config.
+     */
+    @Override
+    public int getStart() {
+        return start;
+    }
+
+    /**
+     * For an incoming config, generate and return all direct neighbors to this
+     * config.
+     *
+     * @param config the incoming config.
+     * @return the collection of neighbor configs.
+     */
+    @Override
+    public ArrayList<Integer> getNeighbors(int config) {
+        ArrayList<Integer> neighbors = new ArrayList<Integer>(2);
+        if (config < 1 || config > total) {
+            return neighbors;
+        }
+        neighbors.add(config == total ? 1 : config + 1);
+        neighbors.add(config == 1 ? total : config - 1);
+        return neighbors;
+    }
+
+    /**
+     * Get the goal config for this puzzle.
+     *
+     * @return the goal config.
+     */
+    @Override
+    public int getGoal() {
+        return goal;
     }
 
 }
