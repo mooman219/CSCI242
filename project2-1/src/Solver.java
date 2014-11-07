b675e5c47d0aa99db52640977b4039eabe49b569
Joe <mooman219@gmail.com>
2014-11-06 20:57:00 -0500

    Document Solver

--- project2-1/src/Solver.java
+++ project2-1/src/Solver.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
 
 /**
+ * This class is designed to solve any puzzle implementing the puzzle interface.
+ *
  * @author Joseph Cumbo (mooman219)
  */
 public class Solver {
 
+    /**
+     * Solves the given puzzle using a naive BFS.
+     *
+     * @param puzzle the puzzle to solve.
+     * @return a list of steps taken to reach the solution. Null if no solution
+     * is found.
+     */
     public ArrayList<Integer> solve(Puzzle puzzle) {
         ArrayList<ArrayList<Integer>> queue = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> current = new ArrayList<Integer>();
