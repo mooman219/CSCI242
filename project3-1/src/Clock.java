9bad7828284041bff6a7faa853b8673f5f970105
Joe <mooman219@gmail.com>
2014-11-19 11:12:25 -0500

    Copy Clock from project2-1

---
+++ project3-1/src/Clock.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+
+/**
+ * This class is a puzzle where it is given a total amount of hours and must
+ * find the shortest path from a starting number to a goal number.
+ *
+ * @author Joseph Cumbo (mooman219)
+ */
+public class Clock implements Puzzle<Integer> {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java Clock hours start goal");
+            return;
+        }
+        int hours, start, goal;
+        try {
+            hours = Integer.parseInt(args[0]);
+            start = Integer.parseInt(args[1]);
+            goal = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("Usage: java Clock hours start goal");
+            return;
+        }
+        Solver solver = new Solver();
+        Clock puzzle = new Clock(hours, start, goal);
+        ArrayList<Integer> solution = solver.solve(puzzle);
+        if (solution == null) {
+            System.out.println("No solution");
+        } else {
+            for (int i = 0; i < solution.size(); i++) {
+                System.out.printf("Step %d: %d\n", i, solution.get(i));
+            }
+        }
+    }
+
+    private final int total;
+    private final int start;
+    private final int goal;
+
+    /**
+     * Initializes a new clock object.
+     *
+     * @param total the total hours in the clock.
+     * @param start the starting hour on the clock.
+     * @param goal the goal hour to reach on the clock.
+     */
+    public Clock(int total, int start, int goal) {
+        this.total = total;
+        this.start = start;
+        this.goal = goal;
+    }
+
+    /**
+     * Get the starting config for this puzzle.
+     *
+     * @return the starting config.
+     */
+    @Override
+    public Integer getStart() {
+        return start;
+    }
+
+    /**
+     * For an incoming config, generate and return all direct neighbors to this
+     * config.
+     *
+     * @param config the incoming config.
+     * @return the collection of neighbor configs.
+     */
+    @Override
+    public ArrayList<Integer> getNeighbors(Integer config) {
+        ArrayList<Integer> neighbors = new ArrayList<Integer>(2);
+        if (config < 1 || config > total) {
+            return neighbors;
+        }
+        neighbors.add(config == total ? 1 : config + 1);
+        neighbors.add(config == 1 ? total : config - 1);
+        return neighbors;
+    }
+
+    /**
+     * Get the goal config for this puzzle.
+     *
+     * @return the goal config.
+     */
+    @Override
+    public Integer getGoal() {
+        return goal;
+    }
+
+}
