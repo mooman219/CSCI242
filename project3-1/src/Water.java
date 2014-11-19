8064eca33adde22bb3fdae47246cdd7532ca4d69
Joe <mooman219@gmail.com>
2014-11-19 15:46:15 -0500

    Implement getNeighbros

--- project3-1/src/Water.java
+++ project3-1/src/Water.java
@@ -51,6 +51,60 @@ public class Water implements Puzzle<Water.JugState> {
     @Override
     public ArrayList<JugState> getNeighbors(JugState config) {
         ArrayList<JugState> neighbors = new ArrayList<JugState>();
+        // Local copy of the jugs
+        int[] localJugs;
+        for (int i = 0; i < config.jugs.length; i++) {
+            // Note: Jugs referenced using 'i' will be called the "current jug"
+
+            // If the current jug isn't full
+            if (config.jugs[i] != maxAmounts[i]) {
+                // Get a copy of the jugs in config
+                localJugs = config.copyJugs();
+                // Then fill the current jug
+                localJugs[i] = maxAmounts[i];
+                // Finally add it as a neighbor
+                neighbors.add(new JugState(localJugs));
+            }
+
+            // If the current jug isn't empty
+            if (config.jugs[i] != 0) {
+                // Get a copy of the jugs in config
+                localJugs = config.copyJugs();
+                // Then empty the current jug
+                localJugs[i] = 0;
+                // Finally add it as a neighbor
+                neighbors.add(new JugState(localJugs));
+
+                for (int x = 0; x < config.jugs.length; x++) {
+                    // Note: Jugs referenced using 'x' will be called the "other jug"
+
+                    // If the current jug isn't the other jug
+                    if (i != x) {
+                        // If the other jug isn't full
+                        if (config.jugs[x] != maxAmounts[x]) {
+                            /**
+                             * At this point, we know the current jug isn't
+                             * empty, we're working with two different jugs, and
+                             * the other jug isn't full. Now we can pour the
+                             * current jug into the other jug.
+                             */
+
+                            // Get a copy of the jugs in config
+                            localJugs = config.copyJugs();
+                            /**
+                             * Since you can't over fill a jug, or pour more
+                             * that you have, get the minimum of the two values
+                             */
+                            int howMuchCanIPour = Math.min(maxAmounts[x] - config.jugs[x], config.jugs[i]);
+                            localJugs[i] -= howMuchCanIPour;
+                            localJugs[i] += howMuchCanIPour;
+                            // Finally add it as a neighbor
+                            neighbors.add(new JugState(localJugs));
+                        }
+                    }
+                }
+            }
+        }
         return neighbors;
     }
 
@@ -78,8 +132,10 @@ public class Water implements Puzzle<Water.JugState> {
             this.jugs = jugs;
         }
 
-        public int[] getJugs() {
-            return jugs;
+        public int[] copyJugs() {
+            int[] jugsCopy = new int[this.jugs.length];
+            System.arraycopy(this.jugs, 0, jugsCopy, 0, this.jugs.length);
+            return jugsCopy;
         }
 
         @Override
@@ -99,5 +155,4 @@ public class Water implements Puzzle<Water.JugState> {
             return true;
         }
     }
-
 }
