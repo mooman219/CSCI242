bedee8642ee50d89c30e58500601c07d568ee566
Joe <mooman219@gmail.com>
2014-11-25 22:29:34 -0500

    Start lab13-3

---
+++ lab13-3/src/DialogViewer.java
@@ -0,0 +1,16 @@
+/**
+ * Prompts the user for some type of input based on the command line arguments.
+ *
+ * @author Joseph Cumbo (mooman219)
+ */
+public class DialogViewer {
+
+    /**
+     * Main method.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
