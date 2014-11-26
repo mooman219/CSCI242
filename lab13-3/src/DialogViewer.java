9890cd398276691a690dafd255433535ec518af8
Joe <mooman219@gmail.com>
2014-11-25 22:36:52 -0500

    Create outline

--- lab13-3/src/DialogViewer.java
+++ lab13-3/src/DialogViewer.java
@@ -11,6 +11,20 @@ public class DialogViewer {
      * @param args command line arguments
      */
     public static void main(String[] args) {
-
+        if (args.length < 1) {
+            System.out.println("Insufficient arguments.");
+            return;
+        }
+        switch (args[0]) {
+            case "f":
+                break;
+            case "c":
+                break;
+            case "m":
+                break;
+            default:
+                System.out.println("Usage: 'f', 'c', or 'm'.");
+                return;
+        }
     }
 }
