76869a78ddf85a08c1f8248414d90c7f5ddaa077
Joe <mooman219@gmail.com>
2014-11-25 22:08:27 -0500

    Start lab13-2

---
+++ lab13-2/src/ScramblePad.java
@@ -0,0 +1,14 @@
+/**
+ * @author Joseph Cumbo (mooman219)
+ */
+public class ScramblePad {
+
+    /**
+     * Main method.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
