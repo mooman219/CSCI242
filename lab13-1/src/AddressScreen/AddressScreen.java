73d694971b885ceb888e79653cb82125c40d7964
Joe <mooman219@gmail.com>
2014-11-25 21:55:45 -0500

    Start lab13-1

---
+++ lab13-1/src/AddressScreen/AddressScreen.java
@@ -0,0 +1,15 @@
+package AddressScreen;
+
+/**
+ * @author Joseph Cumbo (mooman219)
+ */
+public class AddressScreen {
+
+    /**
+     * Main method.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
